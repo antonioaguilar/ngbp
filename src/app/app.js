c35122842f65570cecda0100f23cf63f70317056
Antonio Aguilar <info@pistagram.com>
2015-04-18 00:52:01 +0100

    updated app.js and default html template

--- src/app/app.js
+++ src/app/app.js
@@ -1,35 +1,21 @@
 /**
  * app.js - Main app
  */
-
 angular.module('ngbp', [
-
-  // html templates
   'app.templates',
-
-  // vendor dependencies
   'ui.router',
   'ngResource',
   'ngCookies',
 
   // Default page
   'app.home'
-
 ])
 
 .config(function myAppConfig ($urlRouterProvider) {
-
   $urlRouterProvider.otherwise('/home');
-
-})
-
-.run(function runApp ($location, $rootScope) {
-
-
 })
 
 .controller('AppCtrl', function AppCtrl ($scope) {
-
   $scope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
     if (angular.isDefined(toState.data.pageTitle)) {
       $scope.pageTitle = toState.data.pageTitle + ' | Website Title';
